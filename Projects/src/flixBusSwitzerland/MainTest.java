632ea5768b3d1a977dfa344f8e6c407bf7293304
zmeieb <ben.meier@bbcag.ch>
2018-12-21 09:40:29 +0100

    add 6 unit tests

--- Projects/src/flixBusSwitzerland/MainTest.java
+++ Projects/src/flixBusSwitzerland/MainTest.java
@@ -1,14 +1,53 @@
 package flixBusSwitzerland;
 
 
-class MainTest {
-/*
-    @org.junit.jupiter.api.Test
-    void main() {
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class MainTest {
+
+    @Test
+    public void createNewTravel(){
+        List<Travel> travelList = new ArrayList<>();
+        Bus bus = new Bus(187, "1st Class");
+        Travel travel = new Travel("Paris", "9:00", "13:00", "International", bus);
+        travelList.add(travel);
+
+        Assert.assertEquals("Paris", travel.getDestination());
+        Assert.assertEquals("9:00", travel.getDepartureTime());
+        Assert.assertEquals("13:00", travel.getArrivalTime());
+        Assert.assertEquals("International", travel.getRegion());
     }
 
-    @org.junit.jupiter.api.Test
-    void creations() {
+    @Test
+    public void createNewBus(){
+        Bus bus = new Bus(150, "Economy");
+
+        Assert.assertEquals("Economy", bus.getComfort());
+        Assert.assertEquals(150, bus.getPassengerCapacity());
     }
-    */
+
+    @Test
+    public void calculateTrip(){
+
+    }
+    @Test
+    public void generateNewTrip(){
+
+    }
+
+    @Test
+    public void calculatePlatforms(){
+
+    }
+
+    @Test
+    public void doubleDeckerTest(){
+
+    }
 }
