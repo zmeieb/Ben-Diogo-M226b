0f761da828142e1364bb8bc4685bd154a9b19a16
diogo_caraca <diogo.caraca@hotmail.com>
2018-12-21 11:32:12 +0100

    Klassendiagramm updatet und Main angepasst

--- Projects/src/flixBusSwitzerland/Main.java
+++ Projects/src/flixBusSwitzerland/Main.java
@@ -9,10 +9,11 @@ package flixBusSwitzerland;
 import java.util.*;
 
 public class Main {
-    private static List<Travel> travelList = createTravels();
+    private static List<Travel> travelList = createTravels(createBuses());
     static boolean stop = true;
     public static void main(String[] args) {
-        Main.creations();
+        createBusTerminal();
+
         while(stop) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("\n\n\nChoose your action:");
@@ -40,138 +41,19 @@ public class Main {
                 break;
 
             default:
-                throw new RuntimeException("User did not write useful answer!");
+                System.out.println("User did not choosed a valid option.");
+                System.out.println("Please try it again.");
+                break;
         }
     }
 
-    public static void creations(){
-        List<Platform> platforms = new List<Platform>() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator<Platform> iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean add(Platform platform) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection<? extends Platform> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(int index, Collection<? extends Platform> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public Platform get(int index) {
-                return null;
-            }
-
-            @Override
-            public Platform set(int index, Platform element) {
-                return null;
-            }
-
-            @Override
-            public void add(int index, Platform element) {
-
-            }
-
-            @Override
-            public Platform remove(int index) {
-                return null;
-            }
-
-            @Override
-            public int indexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public int lastIndexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public ListIterator<Platform> listIterator() {
-                return null;
-            }
-
-            @Override
-            public ListIterator<Platform> listIterator(int index) {
-                return null;
-            }
-
-            @Override
-            public List<Platform> subList(int fromIndex, int toIndex) {
-                return null;
-            }
-        };
-
-        //Buses Creation
-        SingleFloorBus singleFloorBusLarge = new SingleFloorBus(100, "1st Class");
-        SingleFloorBus singleFloorBusSmall = new SingleFloorBus(50, "Basic");
-        DoubleDecker doubleDeckerLarge = new DoubleDecker(150, "1st Class");
-        DoubleDecker doubleDeckerSmall = new DoubleDecker(75, "Basic");
+    private static BusTerminal createBusTerminal(){
+        List<Platform> platforms = new ArrayList<>();
         //Platform Creation
-        Platform platform1 = new Platform(1, 6, doubleDeckerLarge.getSERVICE(), "large", true);
-        Platform platform2 = new Platform(2, 6, singleFloorBusLarge.getSERVICE(), "large", false);
-        Platform platform3 = new Platform(3, 3, doubleDeckerSmall.getSERVICE(), "small", false);
-        Platform platform4 = new Platform(4, 3, singleFloorBusSmall.getSERVICE(), "small", true);
+        Platform platform1 = new Platform(1, 6, "international", "large", true);
+        Platform platform2 = new Platform(2, 6, "national", "large", false);
+        Platform platform3 = new Platform(3, 3, "international", "small", false);
+        Platform platform4 = new Platform(4, 3, "national", "small", true);
         //adding Platforms
         platforms.add(platform1);
         platforms.add(platform2);
@@ -180,24 +62,29 @@ public class Main {
         //Bus Terminal Creation
         BusTerminal busTerminal = new BusTerminal("Bus Terminal Zürich", platforms);
 
-        Travel travel1 = new Travel("Basel", "5:30", "7:00", "national", singleFloorBusSmall);
-        Travel travel2 = new Travel("Crikvenica", "19:30", "7:30", "international", doubleDeckerLarge);
-        Travel travel3 = new Travel("Milan", "14:15", "18:45", "international", doubleDeckerSmall);
-        Travel travel4 = new Travel("Genf", "12:00", "15:00", "national", singleFloorBusLarge);
-
+        return busTerminal;
     }
-    private static List<Travel> createTravels(){
-        List<Travel> travelList = new ArrayList<>();
+    private static List<Bus> createBuses(){
+        List<Bus> busList = new ArrayList<>();
 
         SingleFloorBus singleFloorBusLarge = new SingleFloorBus(100, "1st Class");
         SingleFloorBus singleFloorBusSmall = new SingleFloorBus(50, "Basic");
         DoubleDecker doubleDeckerLarge = new DoubleDecker(150, "1st Class");
         DoubleDecker doubleDeckerSmall = new DoubleDecker(75, "Basic");
+        busList.add(singleFloorBusLarge);
+        busList.add(singleFloorBusSmall);
+        busList.add(doubleDeckerLarge);
+        busList.add(doubleDeckerSmall);
 
-        Travel travel1 = new Travel("Basel", "5:30", "7:00", "national", singleFloorBusSmall);
-        Travel travel2 = new Travel("Crikvenica", "19:30", "7:30", "international", doubleDeckerLarge);
-        Travel travel3 = new Travel("Milan", "14:15", "18:45", "international", doubleDeckerSmall);
-        Travel travel4 = new Travel("Genf", "12:00", "15:00", "national", singleFloorBusLarge);
+        return busList;
+    }
+    private static List<Travel> createTravels(List<Bus> busList){
+        List<Travel> travelList = new ArrayList<>();
+
+        Travel travel1 = new Travel("Basel", "5:30", "7:00", "national", busList.get(1));
+        Travel travel2 = new Travel("Crikvenica", "19:30", "7:30", "international", busList.get(2));
+        Travel travel3 = new Travel("Milan", "14:15", "18:45", "international", busList.get(3));
+        Travel travel4 = new Travel("Genf", "12:00", "15:00", "national", busList.get(0));
         travelList.add(travel1);
         travelList.add(travel2);
         travelList.add(travel3);
@@ -214,18 +101,32 @@ public class Main {
         String departureTime = scanner.nextLine();
         System.out.println("When should be the arrival time? ");
         String arrivalTime = scanner.nextLine();
-        System.out.println("What kind of region is the Travel? ");
+        System.out.println("What kind of region is the Travel? [national/international]");
         String region = scanner.nextLine();
-        System.out.println("What kind of Comfort would you like to have in your bus? ");
+        System.out.println("What kind of Comfort would you like to have in your bus? [1st Class/basic]");
         String comfort = scanner.nextLine();
-        System.out.println("How much capacity has the bus? ");
-        String capacityString = scanner.nextLine();
-        int capacity = Integer.valueOf(capacityString);
-        Bus bus = new Bus(capacity, comfort);
-        Travel travel = new Travel(destination, departureTime, arrivalTime, region, bus);
-        travelList.add(travel);
-        System.out.println("Created new Trip succesfully");
+        System.out.println("What capacity should the bus have?");
+        int capacity = scanner.nextInt();
 
+        if (region.equals("national")) {
+            Bus bus = new SingleFloorBus(capacity, comfort);
+            Travel travel = new Travel(destination, departureTime, arrivalTime, region, bus);
+            if (travelList.add(travel)){
+                System.out.println("Created new Trip succesfully");
+            }
+        } else if (region.equals("international")) {
+            Bus bus = new DoubleDecker(capacity, comfort);
+            Travel travel = new Travel(destination, departureTime, arrivalTime, region, bus);
+            if (travelList.add(travel)){
+                System.out.println("Created new Trip succesfully");
+            }
+        } else {
+            Bus bus = new Bus(capacity, comfort);
+            Travel travel = new Travel(destination, departureTime, arrivalTime, region, bus);
+            if (travelList.add(travel)){
+                System.out.println("Created new Trip succesfully");
+            }
+        }
     }
 
     private static void viewDepatures(){
