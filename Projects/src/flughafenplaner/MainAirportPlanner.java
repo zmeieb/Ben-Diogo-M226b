00660505567c4c72c98647a92e3547e443ab319f
zmeieb <ben.meier@bbcag.ch>
2018-12-24 13:17:23 +0100

    add main method

--- Projects/src/flughafenplaner/MainAirportPlanner.java
+++ Projects/src/flughafenplaner/MainAirportPlanner.java
@@ -2,11 +2,11 @@ package flughafenplaner;
 
 public class MainAirportPlanner {
 
-    public void main(String[]args){
+    public static void main(String[]args){
 
 
         //TODO: For Diögeli: Mir chönntet au no e Klass Passagier mache wo itreit wird und denn d'Klass Gepäck brüchti will sis Gepäck es gewicht het
-
+        System.out.println("Wer das liest ist gay");
 
     }
 }
