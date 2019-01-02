f30d6f4a05ae7c2df9348af520f51f27cdfcda9e
zmeieb <ben.meier@bbcag.ch>
2019-01-02 20:47:10 +0100

    create class for Airportplannner

--- Projects/src/fussballManager/CoTrainer.java
+++ Projects/src/fussballManager/CoTrainer.java
@@ -5,7 +5,7 @@ public class CoTrainer extends Trainer {
     CoTrainer(String name, int age, int trainExperience, String rank) {
         super(name, age, trainExperience);
     }
-        //This is a Test Merge
+        //This is a Test Merge and digo is literally gay
     @Override
     public void trainDefense() {
 
